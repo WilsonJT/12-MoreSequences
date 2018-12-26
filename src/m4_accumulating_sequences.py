1bd06083e5ce63ace136297c5e9bfd16fb00a4e0
wilsonjt <wilsonjt@rose-hulman.edu>
2018-12-26 17:24:03 -0500

    Fixed my problem!

--- src/m4_accumulating_sequences.py
+++ src/m4_accumulating_sequences.py
@@ -404,6 +404,7 @@ def rectangles_from_circles(circles):
         rect = rg.Rectangle(bot_left, top_right)
         rectangles.append(rect)
     return rectangles
+
     # -------------------------------------------------------------------------
     # DONE: 10. Implement and test this function.
     #     The testing code is already written for you (above).
